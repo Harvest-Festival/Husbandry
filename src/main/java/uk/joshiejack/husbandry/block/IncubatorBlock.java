78b4a9c8fce8d8d565c4d52b2b8a9fea2a576e1a
joshiejack <joshjackwildman@gmail.com>
2021-06-03 23:07:56 +0100

    Fix incubator bounding box

--- src/main/java/uk/joshiejack/husbandry/block/IncubatorBlock.java
+++ src/main/java/uk/joshiejack/husbandry/block/IncubatorBlock.java
@@ -1,6 +1,7 @@
 package uk.joshiejack.husbandry.block;
 
 import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
@@ -9,7 +10,6 @@ import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
-import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 import uk.joshiejack.husbandry.tileentity.IncubatorTileEntity;
 import uk.joshiejack.penguinlib.block.base.AbstractRotatableBlock;
@@ -21,10 +21,10 @@ import java.util.EnumMap;
 public class IncubatorBlock extends AbstractRotatableBlock {
     private static final EnumMap<Direction, VoxelShape> SHAPES = new EnumMap<>(Direction.class);
     static {
-        SHAPES.put(Direction.NORTH, VoxelShapes.box(0.05D, 0D, 0.3D, 0.95D, 0.7D, 0.95));
-        SHAPES.put(Direction.SOUTH, VoxelShapes.box(0.05D, 0D, 0.05D, 0.95D, 0.7D, 0.7));
-        SHAPES.put(Direction.WEST, VoxelShapes.box(0.3D, 0D, 0.05D, 0.95D, 0.7D, 0.95));
-        SHAPES.put(Direction.EAST, VoxelShapes.box(0.05D, 0D, 0.05D, 0.7D, 0.7D, 0.95));
+        SHAPES.put(Direction.NORTH, Block.box(1D, 0D, 3D, 15D, 11D, 13D));
+        SHAPES.put(Direction.SOUTH, Block.box(1D, 0D, 3D, 15D, 11D, 13D));
+        SHAPES.put(Direction.WEST, Block.box(3D, 0D, 1D, 13D, 11D, 15D));
+        SHAPES.put(Direction.EAST, Block.box(3D, 0D, 1D, 13D, 11D, 15D));
     }
 
     public IncubatorBlock() {
